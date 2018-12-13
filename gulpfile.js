8d342a04f5f8986857535c2275eeafc1a3a6d684
Viktor Persson <viktor.persson@arcsin.se>
2018-12-13 18:19:45 +0100

    Update gulpfile for gulp 4.x and reformat code using prettier.

--- gulpfile.js
+++ gulpfile.js
@@ -1,81 +1,84 @@
-'use strict';
+"use strict";
 
-const gulp = require('gulp');
-const autoprefixer = require('autoprefixer');
-const changed = require('gulp-changed');
-const clean = require('gulp-clean');
-const concat = require('gulp-concat');
-const flexbugsFixes = require('postcss-flexbugs-fixes');
-const fancyLog = require('fancy-log');
-const imagemin = require('gulp-imagemin');
-const postcss = require('gulp-postcss');
-const rev = require('gulp-rev');
-const sass = require('gulp-sass');
-const tildeImporter = require('node-sass-tilde-importer');
+const autoprefixer = require("autoprefixer");
+const changed = require("gulp-changed");
+const clean = require("gulp-clean");
+const concat = require("gulp-concat");
+const fancyLog = require("fancy-log");
+const flexbugsFixes = require("postcss-flexbugs-fixes");
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const postcss = require("gulp-postcss");
+const rev = require("gulp-rev");
+const sass = require("gulp-sass");
+const tildeImporter = require("node-sass-tilde-importer");
 
-const paths = require('./gulpfile.paths');
+const paths = require("./gulpfile.paths");
 let gulpSrcOptions = { read: false };
 let gulpCleanOptions = { force: true };
 
 function catchError(e) {
   fancyLog.error(e);
-  this.emit('end');
+  this.emit("end");
 }
 
 // styles
-gulp.task('clean:styles:prod', function() {
+gulp.task("clean:styles:prod", function() {
   return gulp
     .src(paths.dist.cssFilesGlob, gulpSrcOptions)
     .pipe(clean(gulpCleanOptions));
 });
 
-gulp.task('build:styles:dev', function() {
+gulp.task("build:styles:dev", function() {
   return gulp
-    .src(paths.src.stylesDir + '*.scss')
+    .src(paths.src.stylesDir + "*.scss")
     .pipe(
       sass({
         importer: tildeImporter,
-        outputStyle: 'compressed'
-      })
-        .on('error', catchError)
+        outputStyle: "compressed"
+      }).on("error", catchError)
     )
-    .pipe(concat('styles.min.css'))
+    .pipe(concat("styles.min.css"))
     .pipe(gulp.dest(paths.site.stylesDir))
-    .on('error', catchError);
+    .on("error", catchError);
 });
 
-gulp.task('build:styles:prod', function() {
+gulp.task("build:styles:prod", function() {
   let cssPostProcessors = [
     flexbugsFixes,
-    autoprefixer({ browsers: ['last 5 versions', '> 5%'] })
+    autoprefixer({ browsers: ["last 5 versions", "> 5%"] })
   ];
 
   return gulp
-    .src(paths.src.stylesDir + '*.scss')
+    .src(paths.src.stylesDir + "*.scss")
     .pipe(
       sass({
         importer: tildeImporter,
-        outputStyle: 'compressed'
-      })
-        .on('error', catchError)
+        outputStyle: "compressed"
+      }).on("error", catchError)
     )
     .pipe(postcss(cssPostProcessors))
-    .pipe(concat('styles.min.css'))
+    .pipe(concat("styles.min.css"))
     .pipe(rev())
     .pipe(gulp.dest(paths.dist.stylesDir))
     .pipe(rev.manifest())
     .pipe(gulp.dest(paths.dist.dataDir))
-    .on('error', fancyLog.error);
+    .on("error", fancyLog.error);
 });
 
+gulp.task(
+  "rebuild:styles:prod",
+  gulp.series("clean:styles:prod", "build:styles:prod")
+);
+
 // images
-gulp.task('clean:images', function() {
+gulp.task("clean:images", function() {
   return gulp
     .src(paths.dist.imageFilesGlob, gulpSrcOptions)
     .pipe(clean(gulpCleanOptions));
 });
 
-gulp.task('build:images', function() {
+gulp.task("build:images", function() {
   const dest_dir = paths.dist.imagesDir;
 
   return gulp
@@ -85,14 +88,14 @@ gulp.task('build:images', function() {
     .pipe(gulp.dest(dest_dir));
 });
 
-gulp.task('watch', ['clean:images', 'build:images', 'build:styles:dev'], function() {
-  gulp.watch(paths.src.imageFilesGlob, ['build:images']);
-  gulp.watch(paths.src.sassFilesGlob, ['build:styles:dev']);
+gulp.task("rebuild:images", gulp.series("clean:images", "build:images"));
+
+gulp.task("watch", function() {
+  gulp.watch(paths.src.imageFilesGlob, gulp.series("build:images"));
+  gulp.watch(paths.src.sassFilesGlob, gulp.series("build:styles:dev"));
 });
 
 // composite and default task
-gulp.task('build', [
-  'clean:images', 'build:images',
-  'clean:styles:prod', 'build:styles:prod'
-]);
-gulp.task('default', ['watch']);
+gulp.task("build", gulp.parallel("rebuild:images", "rebuild:styles:prod"));
+gulp.task("dev", gulp.series("rebuild:images", "build:styles:dev", "watch"));
+gulp.task("default", gulp.series("dev"));
