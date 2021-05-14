42ac7d7a32a81903b301122a96ee6db6b0d8bbce
Viktor Persson <viktor.persson@arcsin.se>
2021-05-14 17:11:38 +0200

    Update browser sync config.

--- bs-config.js
+++ bs-config.js
@@ -1,117 +1,22 @@
-/*
- |--------------------------------------------------------------------------
- | Browser-sync config file
- |--------------------------------------------------------------------------
- |
- | For up-to-date information about the options:
- |   http://www.browsersync.io/docs/options/
- |
- | There are more options than you see here, these are just the ones that are
- | set internally. See the website for more info.
- |
- |
- */
+// http://www.browsersync.io/docs/options/
+
 module.exports = {
-  "ui": {
-    "port": 3001
-  },
-  "files": [
-    "public/assets/css/styles.css",
-    "public/assets/img/**/*"
-  ],
-  "watchEvents": [
-    "change"
-  ],
-  "watch": false,
-  "ignore": [],
-  "single": false,
-  "watchOptions": {
-    "ignoreInitial": true
-  },
-  "server": "public",
-  "proxy": false,
-  "port": 3000,
-  "middleware": false,
-  "serveStatic": [],
-  "ghostMode": {
-    "clicks": true,
-    "scroll": true,
-    "location": true,
-    "forms": {
-      "submit": true,
-      "inputs": true,
-      "toggles": true
-    }
-  },
-  "logLevel": "info",
-  "logPrefix": "Browsersync",
-  "logConnections": false,
-  "logFileChanges": true,
-  "logSnippet": true,
-  "rewriteRules": [],
-  "open": false,
-  "browser": "default",
-  "cors": false,
-  "xip": false,
-  "hostnameSuffix": false,
-  "reloadOnRestart": true,
-  "notify": false,
-  "scrollProportionally": true,
-  "scrollThrottle": 0,
-  "scrollRestoreTechnique": "window.name",
-  "scrollElements": [],
-  "scrollElementMapping": [],
-  "reloadDelay": 0,
-  "reloadDebounce": 0,
-  "reloadThrottle": 0,
-  "plugins": [
+  ui: false,
+  files: ["public/assets/**/*"],
+  server: "public",
+  open: false,
+  reloadOnRestart: true,
+  notify: false,
+  reloadDelay: 0,
+  reloadDebounce: 250,
+  reloadThrottle: 0,
+  plugins: [
     {
       module: "bs-html-injector",
       options: {
-        files: "public/**/*.html"
-      }
-    }
-  ],
-  "injectChanges": true,
-  "startPath": null,
-  "minify": true,
-  "host": null,
-  "localOnly": false,
-  "codeSync": true,
-  "timestamps": true,
-  "clientEvents": [
-    "scroll",
-    "scroll:element",
-    "input:text",
-    "input:toggles",
-    "form:submit",
-    "form:reset",
-    "click"
-  ],
-  "socket": {
-    "socketIoOptions": {
-      "log": false
+        files: "public/**/*.html",
+      },
     },
-    "socketIoClientConfig": {
-      "reconnectionAttempts": 50
-    },
-    "path": "/browser-sync/socket.io",
-    "clientPath": "/browser-sync",
-    "namespace": "/browser-sync",
-    "clients": {
-      "heartbeatTimeout": 5000
-    }
-  },
-  "tagNames": {
-    "less": "link",
-    "scss": "link",
-    "css": "link",
-    "jpg": "img",
-    "jpeg": "img",
-    "png": "img",
-    "svg": "img",
-    "gif": "img",
-    "js": "script"
-  },
-  "injectNotification": false
+  ],
+  minify: false,
 };
